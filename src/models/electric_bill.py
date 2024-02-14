88f14e2d9ee9f28d335c399a6c9c2218990f8531
Guy Khmelnitsky <3136012+GuyKh@users.noreply.github.com>
2024-02-14 11:30:56 +0200

    Add Device Endpoint (#29)
    * Add Device endpoint
    
    * Add Invoice request

--- src/models/electric_bill.py
+++ src/models/electric_bill.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass, field
 
 from mashumaro import DataClassDictMixin, field_options
 
+from src.models.invoice import Invoice
 from src.models.response_descriptor import ResponseDescriptor
 
 # GET https://iecapi.iec.co.il//api/ElectricBillsDrawers/ElectricBills/{contract_id}/{bp_number}
@@ -39,29 +40,6 @@ from src.models.response_descriptor import ResponseDescriptor
 #         "description": null
 #     }
 # }
-
-
-
-
-@dataclass
-class Invoice(DataClassDictMixin):
-    full_date: str = field(metadata=field_options(alias="fullDate"))
-    from_date: str = field(metadata=field_options(alias="fromDate"))
-    to_date: str = field(metadata=field_options(alias="toDate"))
-    amount_origin: float = field(metadata=field_options(alias="amountOrigin"))
-    amount_to_pay: float = field(metadata=field_options(alias="amountToPay"))
-    amount_paid: float = field(metadata=field_options(alias="amountPaid"))
-    invoice_id: int = field(metadata=field_options(alias="invoiceId"))
-    contract_number: int = field(metadata=field_options(alias="contractNumber"))
-    order_number: int = field(metadata=field_options(alias="orderNumber"))
-    last_date: str = field(metadata=field_options(alias="lastDate"))
-    invoice_payment_status: int = field(
-        metadata=field_options(alias="invoicePaymentStatus")
-    )
-    document_id: str = field(metadata=field_options(alias="documentId"))
-    days_period: str = field(metadata=field_options(alias="daysPeriod"))
-    has_direct_debit: bool = field(metadata=field_options(alias="hasDirectDebit"))
-    invoice_type: int = field(metadata=field_options(alias="invoiceType"))
 
 
 @dataclass
