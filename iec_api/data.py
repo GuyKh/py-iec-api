4a26d2bd304464eb89ab2d60d7b4ea60b66da976
Guy Khmelnitsky <3136012+GuyKh@users.noreply.github.com>
2024-02-25 08:19:08 +0200

    feat!: Move to async calls (#56)
    * feat!: Move to async model
    
    * fix: typo
    
    * Revert "fix: typo"
    
    This reverts commit 88d27d354cfb510a90432c4311ffa818e08f6178.
    
    * feat: add e2e-test for data + add .coveragerc
    
    * feat: add default value for session and automatic shutdown
    
    * feat: change session shutdown action only to automatic creation
    
    * feat: clean e2e test
    
    * fix: Fix wrong error parsing
    
    * fix: Fix login flow
    
    * chore: bump to 0.2.0

--- iec_api/data.py
+++ iec_api/data.py
@@ -2,10 +2,10 @@ import logging
 from datetime import datetime
 from typing import Optional, TypeVar
 
-import requests
+from aiohttp import ClientSession
 from mashumaro.codecs import BasicDecoder
 
-from iec_api.commons import add_jwt_to_headers
+from iec_api import commons
 from iec_api.const import (
     GET_ACCOUNTS_URL,
     GET_BILLING_INVOICES,
@@ -38,21 +38,15 @@ from iec_api.models.jwt import JWT
 from iec_api.models.meter_reading import MeterReadings
 from iec_api.models.meter_reading import decoder as meter_reading_decoder
 from iec_api.models.remote_reading import ReadingResolution, RemoteReadingRequest, RemoteReadingResponse
-from iec_api.models.response_descriptor import ErrorResponseDescriptor, ResponseWithDescriptor
+from iec_api.models.response_descriptor import ResponseWithDescriptor
 
 logger = logging.getLogger(__name__)
-
-
-def _get_url(url, headers):
-    logger.debug("HTTP GET: %s", url)
-    return requests.get(url=url, headers=headers, timeout=10)
-
 
 T = TypeVar("T")
 
 
-def _get_response_with_descriptor(
-    jwt_token: JWT, request_url: str, decoder: BasicDecoder[ResponseWithDescriptor[T]]
+async def _get_response_with_descriptor(
+    session: ClientSession, jwt_token: JWT, request_url: str, decoder: BasicDecoder[ResponseWithDescriptor[T]]
 ) -> T:
     """
     A function to retrieve a response with a descriptor using a JWT token and a URL.
@@ -64,17 +58,10 @@ def _get_response_with_descriptor(
     Returns:
         T: The response with a descriptor, with its type specified by the return type annotation.
     """
-    headers = add_jwt_to_headers(HEADERS_WITH_AUTH, jwt_token.id_token)
-    response = _get_url(request_url, headers)
+    headers = commons.add_jwt_to_headers(HEADERS_WITH_AUTH, jwt_token.id_token)
+    response = await commons.send_get_request(session=session, url=request_url, headers=headers)
 
-    if response.status_code != 200:
-        if response.content:
-            login_error_response = ErrorResponseDescriptor.from_dict(response.json())
-            raise IECError(login_error_response.code, login_error_response.error)
-        else:
-            raise IECError(response.status_code, response.reason)
-
-    response_with_descriptor = decoder.decode(response.json())
+    response_with_descriptor = decoder.decode(response)
 
     if not response_with_descriptor.data and not response_with_descriptor.response_descriptor.is_success:
         raise IECError(
@@ -84,29 +71,23 @@ def _get_response_with_descriptor(
     return response_with_descriptor.data
 
 
-def get_accounts(token: JWT) -> list[Account]:
+async def get_accounts(session: ClientSession, token: JWT) -> list[Account]:
     """Get Accounts response from IEC API."""
-    return _get_response_with_descriptor(token, GET_ACCOUNTS_URL, account_decoder)
+    return await _get_response_with_descriptor(session, token, GET_ACCOUNTS_URL, account_decoder)
 
 
-def get_customer(token: JWT) -> Optional[Customer]:
+async def get_customer(session: ClientSession, token: JWT) -> Optional[Customer]:
     """Get customer data response from IEC API."""
-    headers = add_jwt_to_headers(HEADERS_WITH_AUTH, token.id_token)
+    headers = commons.add_jwt_to_headers(HEADERS_WITH_AUTH, token.id_token)
     # sending get request and saving the response as response object
-    response = _get_url(url=GET_CONSUMER_URL, headers=headers)
+    response = await commons.send_get_request(session=session, url=GET_CONSUMER_URL, headers=headers)
 
-    if response.status_code != 200:
-        if len(response.content) > 0:
-            login_error_response = ErrorResponseDescriptor.from_dict(response.json())
-            raise IECError(login_error_response.code, login_error_response.error)
-        else:
-            raise IECError(response.status_code, response.reason)
-
-    logger.debug("Response: %s", response.json())
-    return Customer.from_dict(response.json())
+    logger.debug("Response: %s", response)
+    return Customer.from_dict(response)
 
 
-def get_remote_reading(
+async def get_remote_reading(
+    session: ClientSession,
     token: JWT,
     contract_id: str,
     meter_serial_number: str,
@@ -115,7 +96,6 @@ def get_remote_reading(
     from_date: datetime,
     resolution: ReadingResolution = ReadingResolution.DAILY,
 ) -> Optional[RemoteReadingResponse]:
-    headers = add_jwt_to_headers(HEADERS_WITH_AUTH, token.id_token)
     req = RemoteReadingRequest(
         meter_serial_number=meter_serial_number,
         meter_code=str(meter_code),
@@ -125,81 +105,80 @@ def get_remote_reading(
     )
 
     url = GET_REQUEST_READING_URL.format(contract_id=contract_id)
+    headers = commons.add_jwt_to_headers(HEADERS_WITH_AUTH, token.id_token)
     logger.debug(f"HTTP POST: {url}\nData:{req.to_dict()}")
 
-    response = requests.post(url=url, json=req.to_dict(), headers=headers, timeout=10)
+    response = await commons.send_post_request(session=session, url=url, headers=headers, json_data=req.to_dict())
 
-    if response.status_code != 200:
-        if response.status_code == 404:
-            return None
-
-        if len(response.content) > 0:
-            login_error_response = ErrorResponseDescriptor.from_dict(response.json())
-            raise IECError(login_error_response.code, login_error_response.error)
-        else:
-            raise IECError(response.status_code, response.reason)
-
-    logger.debug("Response: %s", response.json())
-    return RemoteReadingResponse.from_dict(response.json())
+    return RemoteReadingResponse.from_dict(response)
 
 
-def get_electric_bill(token: JWT, bp_number: str, contract_id: str) -> ElectricBill:
+async def get_electric_bill(session: ClientSession, token: JWT, bp_number: str, contract_id: str) -> ElectricBill:
     """Get Electric Bill data response from IEC API."""
-    return _get_response_with_descriptor(
-        token, GET_ELECTRIC_BILL_URL.format(contract_id=contract_id, bp_number=bp_number), electric_bill_decoder
+    return await _get_response_with_descriptor(
+        session,
+        token,
+        GET_ELECTRIC_BILL_URL.format(contract_id=contract_id, bp_number=bp_number),
+        electric_bill_decoder,
     )
 
 
-def get_default_contract(token: JWT, bp_number: str) -> Contract:
+async def get_default_contract(session: ClientSession, token: JWT, bp_number: str) -> Contract:
     """Get Contract data response from IEC API."""
-    return _get_response_with_descriptor(token, GET_DEFAULT_CONTRACT_URL.format(bp_number=bp_number), contract_decoder)
+    return await _get_response_with_descriptor(
+        session, token, GET_DEFAULT_CONTRACT_URL.format(bp_number=bp_number), contract_decoder
+    )
 
 
-def get_contracts(token: JWT, bp_number: str) -> Contracts:
+async def get_contracts(session: ClientSession, token: JWT, bp_number: str) -> Contracts:
     """Get all user's Contracts from IEC API."""
-    return _get_response_with_descriptor(token, GET_CONTRACTS_URL.format(bp_number=bp_number), contract_decoder)
+    return await _get_response_with_descriptor(
+        session, token, GET_CONTRACTS_URL.format(bp_number=bp_number), contract_decoder
+    )
 
 
-def get_last_meter_reading(token: JWT, bp_number: str, contract_id: str) -> MeterReadings:
+async def get_last_meter_reading(session: ClientSession, token: JWT, bp_number: str, contract_id: str) -> MeterReadings:
     """Get Last Meter Reading data response from IEC API."""
-    return _get_response_with_descriptor(
-        token, GET_LAST_METER_READING_URL.format(contract_id=contract_id, bp_number=bp_number), meter_reading_decoder
+    return await _get_response_with_descriptor(
+        session,
+        token,
+        GET_LAST_METER_READING_URL.format(contract_id=contract_id, bp_number=bp_number),
+        meter_reading_decoder,
     )
 
 
-def get_devices(token: JWT, contract_id: str) -> list[Device]:
+async def get_devices(session: ClientSession, token: JWT, contract_id: str) -> list[Device]:
     """Get Device data response from IEC API."""
-    headers = add_jwt_to_headers(HEADERS_WITH_AUTH, token.id_token)
+    headers = commons.add_jwt_to_headers(HEADERS_WITH_AUTH, token.id_token)
     # sending get request and saving the response as response object
-    response = _get_url(url=GET_DEVICES_URL.format(contract_id=contract_id), headers=headers)
+    response = await commons.send_get_request(
+        session=session, url=GET_DEVICES_URL.format(contract_id=contract_id), headers=headers
+    )
 
-    if response.status_code != 200:
-        if len(response.content) > 0:
-            login_error_response = ErrorResponseDescriptor.from_dict(response.json())
-            raise IECError(login_error_response.code, login_error_response.error)
-        else:
-            raise IECError(response.status_code, response.reason)
-
-    logger.debug("Response: %s", response.json())
-    return [Device.from_dict(device) for device in response.json()]
+    logger.debug("Response: %s", response)
+    return [Device.from_dict(device) for device in response]
 
 
-def get_device_by_device_id(token: JWT, contract_id: str, device_id: str) -> Devices:
+async def get_device_by_device_id(session: ClientSession, token: JWT, contract_id: str, device_id: str) -> Devices:
     """Get Device data response from IEC API."""
-    return _get_response_with_descriptor(
-        token, GET_DEVICE_BY_DEVICE_ID_URL.format(device_id=device_id, contract_id=contract_id), devices_decoder
+    return await _get_response_with_descriptor(
+        session,
+        token,
+        GET_DEVICE_BY_DEVICE_ID_URL.format(device_id=device_id, contract_id=contract_id),
+        devices_decoder,
     )
 
 
-def get_device_type(token: JWT, bp_number: str, contract_id: str) -> DeviceType:
+async def get_device_type(session: ClientSession, token: JWT, bp_number: str, contract_id: str) -> DeviceType:
     """Get Device Type data response from IEC API."""
-    return _get_response_with_descriptor(
-        token, GET_DEVICE_TYPE_URL.format(bp_number=bp_number, contract_id=contract_id), device_type_decoder
+    # sending get request and saving the response as response object
+    return await _get_response_with_descriptor(
+        session, token, GET_DEVICE_TYPE_URL.format(bp_number=bp_number, contract_id=contract_id), device_type_decoder
     )
 
 
-def get_billing_invoices(token: JWT, bp_number: str, contract_id: str) -> GetInvoicesBody:
+async def get_billing_invoices(session: ClientSession, token: JWT, bp_number: str, contract_id: str) -> GetInvoicesBody:
     """Get Device Type data response from IEC API."""
-    return _get_response_with_descriptor(
-        token, GET_BILLING_INVOICES.format(bp_number=bp_number, contract_id=contract_id), invoice_decoder
+    return await _get_response_with_descriptor(
+        session, token, GET_BILLING_INVOICES.format(bp_number=bp_number, contract_id=contract_id), invoice_decoder
     )
--- iec_api/login.py
+++ iec_api/login.py
@@ -7,10 +7,12 @@ import time
 from logging import getLogger
 from typing import Optional, Tuple
 
+import aiofiles
 import jwt
 import pkce
-import requests
+from aiohttp import ClientSession
 
+from iec_api import commons
 from iec_api.models.exceptions import IECLoginError
 from iec_api.models.jwt import JWT
 
@@ -21,50 +23,62 @@ code_verifier, code_challenge = pkce.generate_pkce_pair()
 STATE = "".join(random.choice(string.digits + string.ascii_letters) for _ in range(12))
 IEC_OKTA_BASE_URL = "https://iec-ext.okta.com"
 
-AUTHORIZE_URL = ("https://iec-ext.okta.com/oauth2/default/v1/authorize?client_id={"
-                 "client_id}&response_type=id_token+code&response_mode=form_post&scope=openid%20email%20profile"
-                 "%20offline_access&redirect_uri=com.iecrn:/&state=123abc&nonce=abc123&code_challenge_method=S256"
-                 "&sessionToken={sessionToken}&code_challenge={challenge}")
+AUTHORIZE_URL = (
+    "https://iec-ext.okta.com/oauth2/default/v1/authorize?client_id={"
+    "client_id}&response_type=id_token+code&response_mode=form_post&scope=openid%20email%20profile"
+    "%20offline_access&redirect_uri=com.iecrn:/&state=123abc&nonce=abc123&code_challenge_method=S256"
+    "&sessionToken={sessionToken}&code_challenge={challenge}"
+)
 
 logger = getLogger(__name__)
 
 
-def authorize_session(session_token) -> str:
+async def authorize_session(session: ClientSession, session_token) -> str:
     """
     Authorizes a session using the provided session token.
     Args:
+        session: The aiohttp ClientSession object.
         session_token (str): The session token to be used for authorization.
     Returns:
         str: The code obtained from the authorization response.
     """
     cmd_url = AUTHORIZE_URL.format(client_id=APP_CLIENT_ID, sessionToken=session_token, challenge=code_challenge)
-    authorize_response = requests.get(cmd_url, timeout=10)
-    code = re.findall(r"<input type=\"hidden\" name=\"code\" value=\"(.+)\"/>",
-                      authorize_response.content.decode('unicode-escape')
-                      .encode('latin1').decode('utf-8'))[0]
+    authorize_response = await commons.send_non_json_get_request(
+        session=session, url=cmd_url, encoding="unicode-escape"
+    )
+    code = re.findall(
+        r"<input type=\"hidden\" name=\"code\" value=\"(.+)\"/>",
+        authorize_response.encode("latin1").decode("utf-8"),
+    )[0]
     return code
 
 
-def get_first_factor_id(user_id: str):
+async def get_first_factor_id(session: ClientSession, user_id: str):
     """
     Function to get the first factor ID using the provided ID.
         Args:
-        id (str): The user ID be used for authorization.
+        session: The aiohttp ClientSession object.
+        user_id (str): The user ID be used for authorization.
     Returns:
         Tuple[str,str]: [The state token, the factor id]
     """
     data = {"username": f"{user_id}@iec.co.il"}
     headers = {"accept": "application/json", "content-type": "application/json"}
 
-    response = requests.post(f"{IEC_OKTA_BASE_URL}/api/v1/authn", data=json.dumps(data), headers=headers, timeout=10)
-    return response.json().get("stateToken", ""), response.json().get("_embedded", {}).get("factors", {})[0].get("id")
+    response = await commons.send_post_request(
+        session=session, url=f"{IEC_OKTA_BASE_URL}/api/v1/authn", json_data=data, headers=headers
+    )
+    return response.get("stateToken", ""), response.get("_embedded", {}).get("factors", {})[0].get("id")
 
 
-def send_otp_code(factor_id: object, state_token: object, pass_code: object = None) -> Optional[str]:
+async def send_otp_code(
+    session: ClientSession, factor_id: object, state_token: object, pass_code: object = None
+) -> Optional[str]:
     """
     Send OTP code for factor verification and return the session token if successful.
 
     Args:
+        session: The aiohttp ClientSession object.
         factor_id (object): The identifier of the factor for verification.
         state_token (object): The state token for the verification process.
         pass_code (object, optional): The pass code for verification. Defaults to None.
@@ -77,19 +91,22 @@ def send_otp_code(factor_id: object, state_token: object, pass_code: object = No
         data["passCode"] = pass_code
     headers = {"accept": "application/json", "content-type": "application/json"}
 
-    response = requests.post(
-        f"{IEC_OKTA_BASE_URL}/api/v1/authn/factors/{factor_id}/verify", data=json.dumps(data),
-        headers=headers, timeout=10
+    response = await commons.send_post_request(
+        session=session,
+        url=f"{IEC_OKTA_BASE_URL}/api/v1/authn/factors/{factor_id}/verify",
+        json_data=data,
+        headers=headers,
     )
-    if response.json().get("status") == "SUCCESS":
-        return response.json().get("sessionToken")
+    if response.get("status") == "SUCCESS":
+        return response.get("sessionToken")
     return None
 
 
-def get_access_token(code) -> JWT:
+async def get_access_token(session: ClientSession, code) -> JWT:
     """
     Get the access token using the provided authorization code.
     Args:
+        session: The aiohttp ClientSession object.
         code (str): The authorization code.
     Returns:
         JWT: The access token as a JWT object.
@@ -101,15 +118,18 @@ def get_access_token(code) -> JWT:
         "redirect_uri": APP_REDIRECT_URI,
         "code": code,
     }
-    response = requests.post(f"{IEC_OKTA_BASE_URL}/oauth2/default/v1/token", data=data, timeout=10)
-    return JWT.from_dict(response.json())
+    response = await commons.send_post_request(
+        session=session, url=f"{IEC_OKTA_BASE_URL}/oauth2/default/v1/token", data=data
+    )
+    return JWT.from_dict(response)
 
 
-def first_login(id_number: str) -> Tuple[str, str, str]:
+async def first_login(session: ClientSession, id_number: str) -> Tuple[str, str, str]:
     """
     Perform the first login for a user.
 
     Args:
+        session: The aiohttp ClientSession object.
         id_number (str): The user's ID number.
 
     Returns:
@@ -118,10 +138,10 @@ def first_login(id_number: str) -> Tuple[str, str, str]:
 
     try:
         # Get the first factor ID and state token
-        state_token, factor_id = get_first_factor_id(id_number)
+        state_token, factor_id = await get_first_factor_id(session, id_number)
 
         # Send OTP code and get session token
-        session_token = send_otp_code(factor_id, state_token)
+        session_token = await send_otp_code(session, factor_id, state_token)
 
         return state_token, factor_id, session_token
     except Exception as error:
@@ -129,11 +149,12 @@ def first_login(id_number: str) -> Tuple[str, str, str]:
         raise IECLoginError(-1, "Failed at first login")
 
 
-def verify_otp_code(factor_id: str, state_token: str, otp_code: str) -> JWT:
+async def verify_otp_code(session: ClientSession, factor_id: str, state_token: str, otp_code: str) -> JWT:
     """
     Verify the OTP code for the given factor_id, state_token, and otp_code and return the JWT.
 
     Args:
+        session: The aiohttp ClientSession object.
         factor_id (str): The factor ID for the OTP verification.
         state_token (str): The state token for the OTP verification.
         otp_code (str): The OTP code to be verified.
@@ -142,34 +163,36 @@ def verify_otp_code(factor_id: str, state_token: str, otp_code: str) -> JWT:
         JWT: The JSON Web Token (JWT) for the authorized session.
     """
     try:
-        otp_session_token = send_otp_code(factor_id, state_token, otp_code)
-        code = authorize_session(otp_session_token)
-        jwt_token = get_access_token(code)
+        otp_session_token = await send_otp_code(session, factor_id, state_token, otp_code)
+        code = await authorize_session(session, otp_session_token)
+        jwt_token = await get_access_token(session, code)
         return jwt_token
     except Exception as error:
         logger.warning("Failed at OTP verification: %s", error)
         raise IECLoginError(-1, "Failed at OTP verification")
 
 
-def manual_authorization(id_number) -> Optional[JWT]:  # pragma: no cover
+async def manual_authorization(session: ClientSession, id_number) -> Optional[JWT]:  # pragma: no cover
     """Get authorization token from IEC API."""
     if not id_number:
-        id_number = input("Enter your ID Number: ")
-    state_token, factor_id, session_token = first_login(id_number)
+        id_number = await commons.read_user_input("Enter your ID Number: ")
+    state_token, factor_id, session_token = await first_login(session, id_number)
     if not state_token:
-        print("Failed to send OTP")
-        return None
+        logger.error("Failed to send OTP")
+        raise IECLoginError(-1, "Failed to send OTP, no state_token")
 
-    otp_code = input("Enter your OTP code: ")
-    code = authorize_session(otp_code)
-    jwt_token = verify_otp_code(factor_id, state_token, code)
-    print(f"Access token: {jwt_token.access_token}\n"
-          f"Refresh token: {jwt_token.refresh_token}\n"
-          f"id_token: {jwt_token.id_token}")
+    otp_code = await commons.read_user_input("Enter your OTP code: ")
+    code = await authorize_session(session, otp_code)
+    jwt_token = await verify_otp_code(session, factor_id, state_token, code)
+    logger.debug(
+        f"Access token: {jwt_token.access_token}\n"
+        f"Refresh token: {jwt_token.refresh_token}\n"
+        f"id_token: {jwt_token.id_token}"
+    )
     return jwt_token
 
 
-def refresh_token(token: JWT) -> Optional[JWT]:
+async def refresh_token(session: ClientSession, token: JWT) -> Optional[JWT]:
     """Refresh IEC JWT token."""
     headers = {"accept": "application/json", "content-type": "application/x-www-form-urlencoded"}
     data = {
@@ -177,35 +200,37 @@ def refresh_token(token: JWT) -> Optional[JWT]:
         "redirect_uri": APP_REDIRECT_URI,
         "refresh_token": token.refresh_token,
         "grant_type": "refresh_token",
-        "scope": "openid email profile offline_access"
+        "scope": "openid email profile offline_access",
     }
-    response = requests.post(f"{IEC_OKTA_BASE_URL}/oauth2/default/v1/token", data=data, headers=headers, timeout=10)
-    if response.status_code == 200:
-        return JWT.from_dict(response.json())
-    return None
+    response = await commons.send_post_request(
+        session=session, url=f"{IEC_OKTA_BASE_URL}/oauth2/default/v1/token", data=data, headers=headers
+    )
+    return JWT.from_dict(response)
 
 
-def save_token_to_file(token: JWT, path: str = "token.json") -> None:
+async def save_token_to_file(token: JWT, path: str = "token.json") -> None:
     """Save token to file."""
-    with open(path, "w", encoding="utf-8") as f:
-        json.dump(token.to_dict(), f)
+    async with aiofiles.open(path, mode="w", encoding="utf-8") as f:
+        await f.write(json.dumps(token.to_dict()))
 
 
 def decode_token(token: JWT) -> dict:
     return jwt.decode(token.id_token, options={"verify_signature": False}, algorithms=["RS256"])
 
 
-def load_token_from_file(path: str = "token.json") -> JWT:
+async def load_token_from_file(path: str = "token.json") -> JWT:
     """Load token from file."""
-    with open(path, "r", encoding="utf-8") as f:
-        jwt_data = JWT.from_dict(json.load(f))
+    async with aiofiles.open(path, "r", encoding="utf-8") as f:
+        contents = await f.read()
 
-        # decode token to verify validity
-        decode_token(jwt_data)
+    jwt_data = JWT.from_dict(json.loads(contents))
 
-        return jwt_data
+    # decode token to verify validity
+    decode_token(jwt_data)
+
+    return jwt_data
 
 
 def get_token_remaining_time_to_expiration(token: JWT):
     decoded_token = decode_token(token)
-    return decoded_token['exp'] - int(time.time())
+    return decoded_token["exp"] - int(time.time())
