9bb4efb2e933983993d492e9b5b6853d42d8456f
Guy Khmelnitsky <3136012+GuyKh@users.noreply.github.com>
2024-02-29 13:56:33 +0200

    chore: Revert Example ID (#67)

--- example.py
+++ example.py
@@ -17,7 +17,7 @@ async def main():
     session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False), timeout=aiohttp.ClientTimeout(total=10))
     try:
         # Example of usage
-        client = IecClient(200461929, session)
+        client = IecClient(123456782, session)
 
         token_json_file = "token.json"
         if os.path.exists(token_json_file):
