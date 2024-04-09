a6a2bfc54d60960663ff21bf289eb9ce0cba1408
Guy Khmelnitsky <guykhmel@gmail.com>
2024-04-09 12:37:12 +0300

    fix: correct serialization

--- iec_api/models/get_pdf.py
+++ iec_api/models/get_pdf.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 
 from mashumaro import DataClassDictMixin, field_options
+from mashumaro.config import BaseConfig
 
 
 @dataclass
@@ -12,3 +13,6 @@ class GetPdfRequest(DataClassDictMixin):
     invoice_number: str = field(metadata=field_options(alias="invoiceNumber"))
     contract_id: str = field(metadata=field_options(alias="contractId"))
     bp_number: str = field(metadata=field_options(alias="bpNumber"))
+
+    class Config(BaseConfig):
+        serialize_by_alias = True
